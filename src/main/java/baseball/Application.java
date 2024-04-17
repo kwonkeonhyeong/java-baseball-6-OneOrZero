739ef363caf4f457270b7b9e7dcba9cb0748f8ad
jini <dbwls20202554@gmail.com>
2024-04-17 17:18:32 +0900

    no message

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,38 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean restart = true;
+        while (restart) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+            Computer computer = new Computer();
+            computer.generateNumber();
+
+            boolean correct = false;
+            while (!correct) {
+                Player player = new Player();
+                try {
+                    player.guessNumber();
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                    return;
+                }
+                correct = computer.checkAnswer(player.getNumber());
+            }
+
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = Console.readLine();
+            int choice = Integer.parseInt(input);
+
+            if (choice != 1) {
+                restart = false;
+            }
+        }
     }
 }
